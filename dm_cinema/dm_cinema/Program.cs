6cef99b926ce16be3e60f18c4fc3eddc13644057
yassineb2411 <146844349+yassineb2411@users.noreply.github.com>
2024-02-26 19:45:26 +0100

    Update Program.cs

--- dm_cinema/dm_cinema/Program.cs
+++ dm_cinema/dm_cinema/Program.cs
@@ -10,12 +10,6 @@ namespace dm_cinema
     {
         static void Main(string[] args)
         {
-            Acteur monActeur = new Acteur("13/06/1942", 1, "Ford", "Harisson");
-            monActeur.ToString();
-
-            Film monFilm = new Film("1982", "Science-fiction", 1, "L'action du film se situe à Los Angeles en 2019 et met en scène Rick Deckard (interprété par Harrison Ford), un ancien policier qui reprend du service pour traquer un groupe de réplicants, des androïdes créés à l'image de l'Homme, menés par l'énigmatique Roy Batty (interprété par Rutger Hauer).", "Blade Runner", monActeur);
-            monFilm.ToString();
-
             Console.WriteLine("DM_ CINEMA");
             Console.WriteLine("----------------------------------------------");
 
